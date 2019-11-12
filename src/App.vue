2012211f74c70817a2dd8c365910695e5c50594c
Zakarya Tolba <zakarya.tolba@ynov.com>
2019-11-12 15:52:44 +0100

    Project init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,159 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="Welcome to Vue.JS Profiles"></hello-world>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+    <users-list></users-list>
+    <users></users>
+
+    <div id="users">
+      <button @click="searchUsers" id="fetch-users" class="btn btn-primary">Fetch users</button>
+      <label>
+        <input class="form-control align-middle d-inline-block" type="text" v-model="filter.name" placeholder="Name Search"
+               style="width: auto;">
+      </label>
+      <div class="input-group align-middle" style="display:inline-block; width: auto;">
+        <input id="gender-male" class="d-none" type="radio" v-model="filter.gender" value="male">
+        <label for="gender-male"><i class="fas fa-mars" style="font-size: 30px"></i></label>
+        <input id="gender-female" class="d-none" type="radio" v-model="filter.gender" value="female">
+        <label for="gender-female"><i class="fas fa-venus" style="font-size: 30px"></i></label>
+        <input id="gender-*" class="d-none" type="radio" v-model="filter.gender" value="*">
+        <label for="gender-*"><i class="fas fa-transgender" style="font-size: 30px"></i></label>
+      </div>
+      <div class="mt-2">Result: {{ usersSorted.length }} / {{ allUsers.length }}</div>
+
+
+      <div class="singleProfile" @click="showUserModal">
+        <table id="tbl-users" class="table table-hover">
+          <thead>
+          <tr>
+            <th></th>
+            <th @click="changeSort('name')">Nom <i class="fas" :class="getNameIcon"></i></th>
+            <th @click="changeSort('email')">Email <i class="fas" :class="getEmailIcon"></i></th>
+            <th>Tel</th>
+            <th @click="changeSort('age')">Age <i class="fas" :class="getAgeIcon"></i></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="u in usersSorted" :key="u">
+            <td><img :src='u.picture.thumbnail'/></td>
+            <td>{{u.name.first}} {{u.name.last}}</td>
+            <td>{{u.email}}</td>
+<!--            <td>{{u.id.value}}</td>-->
+            <td>{{u.phone}}</td>
+            <td>{{u.dob.age}}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <userModal v-show="isModalVisible" @close="closeUserModal">
+
+    </userModal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersList from './components/UsersList.vue';
+import Users from './components/Users.vue';
+import axios from "axios";
+import userModal from './components/userModal.vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
+    UsersList,
+    Users,
+    userModal,
+  },
+data(){
+    return{
+            isModalVisible: false,
+            allUsers: [],
+            sort: {
+              name: 0,
+              email: 0,
+              age: 0,
+            },
+            filter: {
+              name: '',
+              gender: '*',
+            },
+            icons: ['fa-sort', 'fa-sort-up', 'fa-sort-down']
+          }
+  },
+  computed: {
+    getNameIcon() {
+      return this.icons[this.sort.name];
+    },
+    getEmailIcon() {
+      return this.icons[this.sort.email];
+    },
+    getAgeIcon() {
+      return this.icons[this.sort.age];
+    },
+    usersSorted() {
+      return this.allUsers
+              .slice(0)
+              .filter(u => {
+                return this.filter.name === ''
+                        || `${u.name.first} ${u.name.last}`.toLowerCase().includes(this.filter.name.toLowerCase());
+              })
+              .filter(u => {
+                return this.filter.gender === '*' || this.filter.gender === u.gender;
+              })
+              .sort(((a, b) => {
+                let compare = 0;
+
+                const aName = `${a.name.first} ${a.name.last}`;
+                const bName = `${b.name.first} ${b.name.last}`;
+
+                if (this.sort.name === 1 && compare === 0) {
+                  compare = aName.localeCompare(bName);
+                } else if (this.sort.name === 2 && compare === 0) {
+                  compare = -aName.localeCompare(bName);
+                }
+
+                if (this.sort.email === 1 && compare === 0) {
+                  compare = a.email.localeCompare(b.email);
+                } else if (this.sort.email === 2 && compare === 0) {
+                  compare = -a.email.localeCompare(b.email);
+                }
+
+                if (this.sort.age === 1 && compare === 0) {
+                  if (a.dob.age > b.dob.age) compare = 1;
+                  else if (a.dob.age < b.dob.age) compare = -1;
+                } else if (this.sort.age === 2 && compare === 0) {
+                  if (a.dob.age > b.dob.age) compare = -1;
+                  else if (a.dob.age < b.dob.age) compare = 1;
+                }
+
+                return compare;
+              }));
+    },
+  },
+  methods: {
+    showUserModal(){
+      this.isModalVisible = true;
+      // this.loadUserDetails(id);
+    },
+    closeUserModal(){
+      this.isModalVisible = false;
+    },
+    // loadUserDetails(id){
+    //   alert(id);
+    // },
+    changeSort(type) {
+      this.sort[type]++;
+      if (this.sort[type] > 2) this.sort[type] = 0;
+    },
+    async searchUsers() {
+      const {data: {results}} = await axios.get('https://randomuser.me/api/?results=150');
+
+      this.allUsers = results;
+    },
+  },
 }
 </script>
 
@@ -25,4 +166,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+th {
+  cursor: pointer;
+  user-select: none;
+}
+
+input + label {
+  margin: 0 6px;
+  color: rgba(0, 0, 0, .5);
+}
+
+input:checked + label {
+  color: black;
+}
+
 </style>
---
+++ src/components/userModal.vue
@@ -0,0 +1,134 @@
+<script>
+    export default {
+        name: 'userModal',
+        methods: {
+            close() {
+                this.$emit('close');
+            },
+            showUserDetails(u){
+                return u.location.city;
+            }
+        },
+        data (){
+            return {
+
+            }
+        }
+    };
+</script>
+<template>
+    <transition name="modal-fade">
+        <div class="modal-backdrop">
+            <div class="userModal"
+                 role="dialog"
+                 aria-labelledby="modalTitle"
+                 aria-describedby="modalDescription"
+            >
+                <header
+                        class="modal-header"
+                        id="modalTitle"
+                >
+                    <slot name="header">
+                        User Profile
+
+                        <button
+                                type="button"
+                                class="btn-close"
+                                @click="close"
+                                aria-label="Close modal"
+                        >
+                            x
+                        </button>
+                    </slot>
+                </header>
+                <section
+                        class="modal-body"
+                        id="modalDescription"
+                >
+                    <slot name="body">
+<!--                        {{ loadUserDetails(u.id.value) }}-->
+                    </slot>
+                </section>
+                <footer class="modal-footer">
+                    <slot name="footer">
+<!--                        I'm the default footer!-->
+                        <br>
+                        <button
+                                type="button"
+                                class="btn-orange"
+                                @click="close"
+                                aria-label="Close modal"
+                        >
+                            Fermer
+                        </button>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<style>
+    .modal-backdrop {
+        position: relative;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.3);
+        /*background-color: transparent;*/
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .userModal {
+        background: #FFFFFF;
+        box-shadow: 2px 2px 20px 1px;
+        overflow-x: auto;
+        display: flex;
+        flex-direction: column;
+        width: 700px;
+        height: 500px;
+    }
+
+    .modal-header,
+    .modal-footer {
+        padding: 15px;
+        display: flex;
+        height: 50px;
+    }
+
+    .modal-header {
+        border-bottom: 1px solid #eeeeee;
+        color: #D78900;
+        justify-content: space-between;
+    }
+
+    .modal-footer {
+        border-top: 1px solid #eeeeee;
+        justify-content: flex-end;
+    }
+
+    .modal-body {
+        position: relative;
+        padding: 20px 10px;
+    }
+
+    .btn-close {
+        border: none;
+        font-size: 25px;
+        cursor: pointer;
+        font-weight: bold;
+        margin-top: -10px;
+        color: #D78900;
+        background: transparent;
+    }
+
+    .btn-orange {
+        color: white;
+        background: #D78900;
+        border: 1px solid #D78900;
+        border-radius: 2px;
+    }
+</style>
